cba2b91e82c849a8fabd28c75c785f45117799e4
Eric Shewchuk <eric@thebanyanteam.com>
2023-10-22 21:25:30 -1000

    built out some animations

--- animations/animation on scroll/scripts/scroll.js
+++ animations/animation on scroll/scripts/scroll.js
@@ -1,23 +1,15 @@
-let gridBottom = 0;
-let gridTop = 0;
-
-document.onload = setScroll();
-    
-    function setScroll() {
-
-    var windowHeight = document.body.clientHeight,
-      gridTop = windowHeight * .1,
-      gridBottom = windowHeight * .9;
+const button = document.querySelectorAll('button');
+const windowHeight = document.body.clientHeight;
+let gridTop = windowHeight * .1;
+let gridBottom = windowHeight * .9;
   
-    $(window).on('scroll', function() {
-      $('button').each(function() {
-        var thisTop = $(this).offset().top - $(window).scrollTop();
-        if (thisTop >= gridTop && (thisTop + $(this).height()) <= gridBottom) {
-          $(this).addClass('animated');
-        } else {
-          $(this).removeClass('animated');
+window.addEventListener('scroll', testButton);
+  
+    function testButton() {
+      for(i=0; i<button.length; i++) {
+        let thisTop = button[i].getBoundingClientRect().top; // gets current position of button top corner
+        if (thisTop < gridBottom) {button[i].classList.add('animated');} // adds 'animated' when it's on screen
+        if (thisTop > gridBottom || thisTop < gridTop) {button[i].classList.remove('animated'); // removes 'animated' when off screen
         }
-      });
-    });
-    $(window).trigger('scroll');
-  };
+      }
+    };
